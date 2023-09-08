262a5a31da26716837d7ca7f5198d5850d242eb1
santiago <108626122+marcosantiagomuro@users.noreply.github.com>
2023-09-08 18:16:30 +0200

    conditions-gp  | added first files for Road Continditions  #1

--- frontend/src/Components/Map/Zoom.tsx
+++ frontend/src/Components/Map/Zoom.tsx
@@ -1,15 +1,17 @@
-import { ZoomControl } from 'react-leaflet';
-import useZoom from './Hooks/useZoom';
+
+import { ZoomControl } from "react-leaflet"
+import useZoom from "./Hooks/useZoom"
 
 const Zoom = () => {
-  const zoom = useZoom();
 
-  return (
-    <>
-      <div className="map-zoom">{zoom}</div>
-      <ZoomControl position="topright" />
-    </>
-  );
-};
+    const zoom = useZoom()
+   
+    return (
+        <>
+        <div className="map-zoom">{zoom}</div>
+        <ZoomControl position='topright'/>
+        </>
+    )
+}
 
-export default Zoom;
+export default Zoom
