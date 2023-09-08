1606793c3c986c99b32547f32cf0bdab372a3299
santiago <108626122+marcosantiagomuro@users.noreply.github.com>
2023-09-08 18:16:30 +0200

    conditions-gp  | added first files for Road Continditions  #1

--- frontend/src/pages/RoadConditions.tsx
+++ frontend/src/pages/RoadConditions.tsx
@@ -1,41 +1,39 @@
-import { useState } from 'react';
-import { Palette } from 'react-leaflet-hotline';
-import { ChartData } from 'chart.js';
 
-import ConditionsMap from '../Components/RoadConditions/ConditionsMap';
-import ConditionsGraph from '../Components/RoadConditions/ConditionsGraph';
+import { useState } from "react";
+import { Palette } from "react-leaflet-hotline";
+import { ChartData } from "chart.js";
 
-import { ConditionType } from '../models/graph';
+import ConditionsMap from "../Components/RoadConditions/ConditionsMap";
+import ConditionsGraph from "../Components/RoadConditions/ConditionsGraph";
 
-import { GraphProvider } from '../context/GraphContext';
+import { ConditionType } from "../models/graph";
 
-import '../css/road_conditions.css';
+import { GraphProvider } from "../context/GraphContext";
+
+import "../css/road_conditions.css";
+
 
 const RoadConditions = () => {
-  const [palette, setPalette] = useState<Palette>([]);
-  const [wayData, setWayData] = useState<ChartData<'line', number[], number>>();
+    
+    const [palette, setPalette] = useState<Palette>([])
+    const [wayData, setWayData] = useState<ChartData<"line", number[], number>>()
 
-  const type: ConditionType = {
-    name: 'IRI',
-    min: 0,
-    max: 10,
-    grid: true,
-    samples: 40,
-  };
+    const type: ConditionType = {
+        name: 'IRI',
+        min: 0,
+        max: 10,
+        grid: true,
+        samples: 40
+    }
 
-  return (
-    <GraphProvider>
-      <div className="road-conditions-wrapper">
-        <ConditionsMap
-          type={type}
-          palette={palette}
-          setPalette={setPalette}
-          setWayData={setWayData}
-        />
-        <ConditionsGraph type={type} palette={palette} data={wayData} />
-      </div>
-    </GraphProvider>
-  );
-};
+    return (
+        <GraphProvider>
+            <div className="road-conditions-wrapper">
+                <ConditionsMap type={type} palette={palette} setPalette={setPalette} setWayData={setWayData} />
+                <ConditionsGraph type={type} palette={palette} data={wayData} />
+            </div>
+        </GraphProvider> 
+    );
+}
 
 export default RoadConditions;
