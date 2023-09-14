4bc9868529195344b6e00c607547a692fb20cba3
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2023-09-14 12:35:46 +0200

    Create mock lib for react-leaflet

---
+++ frontend/src/Components/Map/MapWrapper.test.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { render, screen } from '@testing-library/react';
+import MapWrapper from './MapWrapper';
+
+test('Check subcomponent of mapwrapper', () => {
+  render(<MapWrapper />);
+
+  const names = ['MapContainer', 'TileLayer', 'ScaleControl', 'ZoomControl'];
+
+  for (let idx in names) {
+    const element = screen.getByText(names[idx]);
+    expect(element).toBeInTheDocument();
+  }
+});
