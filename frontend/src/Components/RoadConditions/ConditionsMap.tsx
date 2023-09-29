120f46ee9d4d3e7b534e7f64ce4952199fdfc4c1
santiago <108626122+marcosantiagomuro@users.noreply.github.com>
2023-09-29 09:56:35 +0200

    test-graph-view | clean code and set map _2

--- frontend/src/Components/RoadConditions/ConditionsMap.tsx
+++ frontend/src/Components/RoadConditions/ConditionsMap.tsx
@@ -1,7 +1,52 @@
-export default function ConditionsMap() {
-  return (
-    <>
-      <h1>hello</h1>
-    </>
-  );
+import { ConditionType } from '../../models/graph';
+import { ChartData } from 'chart.js';
+import MapWrapper from '../Map/MapWrapper';
+import Ways from './Ways';
+import { FC, useCallback, useRef } from 'react';
+//import useSize from '../../hooks/useSize';
+import { getConditions } from '../../queries/conditions';
+import { Condition } from '../../models/path';
+
+interface Props {
+  type: ConditionType;
+  setWayData: React.Dispatch<
+    React.SetStateAction<ChartData<'line', number[], number> | undefined>
+  >;
 }
+
+const ConditionsMap: FC<Props> = ({ type, setWayData }) => {
+  const { name } = type;
+
+  const ref = useRef(null);
+
+  const onClick = useCallback((way_id: string, way_length: number) => {
+    console.log('onclick called');
+
+    getConditions(way_id, name, (wc: Condition[]) => {
+      console.log('getConditions called');
+      setWayData({
+        labels: wc.map((p) => p.way_dist * way_length),
+        datasets: [
+          {
+            type: 'line' as const,
+            label: way_id,
+            borderColor: 'rgb(255, 99, 132)',
+            borderWidth: 2,
+            fill: false,
+            tension: 0.1,
+            data: wc.map((p) => p.value),
+          },
+        ],
+      });
+    });
+  }, []);
+
+  return (
+    <div className="road-conditions-map" ref={ref}>
+      <MapWrapper>
+        <Ways type={name} onClick={onClick} />
+      </MapWrapper>
+    </div>
+  );
+};
+export default ConditionsMap;
--- frontend/src/pages/RoadConditions.tsx
+++ frontend/src/pages/RoadConditions.tsx
@@ -1,40 +1,34 @@
 import { useState } from 'react';
 import { ChartData } from 'chart.js';
 //
-// import ConditionsMap from '../Components/RoadConditions/ConditionsMap';
-// import ConditionsGraph from '../Components/RoadConditions/ConditionsGraph';
-//
-// // import { ConditionType } from '../models/graph';
-//
-// import { GraphProvider } from '../context/GraphContext';
-//
-// import '../css/road_conditions.css';
-//
-// //this is to visualise the Road Conditions (GP) map
-// const RoadConditions = () => {
-//   // const [wayData, setWayData] = useState<ChartData<'line', number[], number>>();
-//
-//  /*const type: ConditionType = {
-//     name: 'IRI',
-//     min: 0,
-//     max: 10,
-//     grid: true,
-//     samples: 40,
-//   };*/
-//
-//   return (
-//     <GraphProvider>
-//       <div className="road-conditions-wrapper">
-//         <ConditionsMap
-//           // type={type}
-//           // setWayData={setWayData}
-//         />
-//         <ConditionsGraph /*type={type} data={wayData}*/ />
-//       </div>
-//     </GraphProvider>
-//   );
-// };
-//
-export default function RoadConditions() {
-  return <>hello2marco</>;
-}
+import ConditionsMap from '../Components/RoadConditions/ConditionsMap';
+import ConditionsGraph from '../Components/RoadConditions/ConditionsGraph';
+
+import { GraphProvider } from '../context/GraphContext';
+
+import '../css/road_conditions.css';
+import { ConditionType } from '../models/graph';
+
+//this is to visualise the Road Conditions (GP) map
+const RoadConditions = () => {
+  const [wayData, setWayData] = useState<ChartData<'line', number[], number>>();
+
+  const type: ConditionType = {
+    name: 'IRI',
+    min: 0,
+    max: 10,
+    grid: true,
+    samples: 40,
+  };
+
+  return (
+    <GraphProvider>
+      <div className="road-conditions-wrapper">
+        <ConditionsMap type={type} setWayData={setWayData} />
+        <ConditionsGraph type={type} data={wayData} />
+      </div>
+    </GraphProvider>
+  );
+};
+
+export default RoadConditions;
