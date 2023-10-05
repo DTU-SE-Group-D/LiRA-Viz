bd183f0f9b1bcd023a79a8aceb7591dbf3cf155e
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2023-10-05 19:33:53 +0200

    Show the Roads on the map

--- frontend/src/Components/RoadDetails/MapArea.tsx
+++ frontend/src/Components/RoadDetails/MapArea.tsx
@@ -1,7 +1,10 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import ImageGallery from './ImageGallery';
 import MapWrapper from '../Map/MapWrapper';
 import ForceMapUpdate from '../Map/ForceMapUpdate';
+import { IRoad } from '../../models/path';
+import { getRoads } from '../../queries/road';
+import Roads from '../Map/Roads';
 
 interface Props {
   triggerUpdate: number;
@@ -10,10 +13,18 @@ interface Props {
  * The map area op the road details(road inspect) page
  */
 const MapArea: React.FC<Props> = ({ triggerUpdate }) => {
+  const [roads, setRoads] = useState<IRoad[]>();
+
+  // get the actual roads
+  useEffect(() => {
+    getRoads(setRoads);
+  }, []);
+
   return (
     <div className="map-area">
       <div className="map_area" style={{ height: '100%' }}>
         <MapWrapper>
+          <Roads roads={roads} />
           <ForceMapUpdate triggerUpdate={triggerUpdate} />
         </MapWrapper>
       </div>
