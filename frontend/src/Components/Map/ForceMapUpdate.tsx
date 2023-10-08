2505497b5d280c42410cdcb27f80a001dbe4d482
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2023-10-08 17:41:12 +0200

    ForceMapUpdate: Allow to force update the map or to go to specific position

---
+++ frontend/src/Components/Map/ForceMapUpdate.tsx
@@ -0,0 +1,31 @@
+import { LatLng } from '../../models/models';
+import { useMap } from 'react-leaflet';
+import React, { useEffect } from 'react';
+
+interface Props {
+  /** A modification of this value is used to trigger an update of the map */
+  triggerUpdate: number;
+  /** The coordinates of the map center*/
+  position?: LatLng;
+}
+
+/**
+ * This component is used to force an update of the map. It is used in conjunction with the useMap hook.
+ */
+const ForceMapUpdate: React.FC<Props> = ({ triggerUpdate, position }) => {
+  const map = useMap();
+
+  useEffect(() => {
+    map.invalidateSize();
+  }, [triggerUpdate]);
+
+  useEffect(() => {
+    if (position) {
+      map.flyTo(position);
+    }
+  }, [position]);
+
+  return null;
+};
+
+export default ForceMapUpdate;
