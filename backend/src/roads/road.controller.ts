274c04b9975969652b15ce6080caab6ad9763803
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2023-11-03 21:18:00 +0100

    Import ways of a road - Get ways from OSM - Insert ways in db
    wip - front
    
    wip - Construct road

--- backend/src/roads/road.controller.ts
+++ backend/src/roads/road.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get } from '@nestjs/common';
+import { BadRequestException, Controller, Get, Param } from '@nestjs/common';
 import { RoadService } from './road.service';
 
 @Controller('road')
@@ -8,5 +8,17 @@ export class RoadController {
   @Get('')
   getRoads() {
     return this.service.getSurveysPath();
+  }
+
+  /**
+   * Add a specific road to the database using the OSM id of a way in the road.
+   * **It might get removed in the future.**
+   *
+   * @param id the OSM id of a way in the road
+   */
+  @Get('getorcreateway/:id')
+  async insertRoad(@Param('id') id: string) {
+    if (isNaN(Number(id))) throw new BadRequestException('Invalid id');
+    return await this.service.getOrCreateWay(id);
   }
 }
