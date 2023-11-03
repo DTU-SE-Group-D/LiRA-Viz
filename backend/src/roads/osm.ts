93cab98865c96286c641d678060fffd2c9010ab2
Seb-sti1 <65665540+seb-sti1@users.noreply.github.com>
2023-11-03 21:18:00 +0100

    Import ways of a road - Get ways from OSM - Insert ways in db
    wip - front
    
    wip - Construct road

---
+++ backend/src/roads/osm.ts
@@ -0,0 +1,155 @@
+/**
+ * This files contains functions to query the OSM API.
+ */
+
+import { IWay } from '../tables';
+import { LatLng } from '../models';
+
+/**
+ * The timeout of the OSM API in seconds.
+ */
+const OSM_API_TIMEOUT = 900;
+
+/**
+ * Queries the OSM API with the given query and calls the callback function with the result.
+ * @param query the query to send to the OSM API
+ * @param callback the callback function to call with the result
+ *
+ * @author Kerbourc'h
+ */
+async function queryOSM<T>(
+  query: string,
+  callback: (data: any) => T,
+): Promise<T> {
+  const controller = new AbortController();
+  const timeoutId = setTimeout(
+    () => controller.abort(),
+    (OSM_API_TIMEOUT + 10) * 1000, // +10 seconds to be sure that the OSM API timeout (and the fetch request)
+  );
+  const result = await fetch('https://overpass-api.de/api/interpreter', {
+    method: 'POST',
+    body: 'data=' + encodeURIComponent(query),
+    signal: controller.signal,
+  })
+    .then(async (data) => {
+      if (data.ok) {
+        const json = await data.json();
+        // If the OSM API responded with a remark, log it.
+        if (json.remark) {
+          if (json.remark.includes('error')) {
+            console.warn('OSM API responded with an error: ', json.remark);
+            return null;
+          } else {
+            console.debug('OSM API responded with a remark: ', json.remark);
+          }
+        }
+        return callback(json);
+      }
+      // If the OSM API responded with an error, log it and return null.
+      console.warn('OSM API responded with an error ', data.status);
+      return null;
+    })
+    .catch((err) => {
+      console.error('Error while querying the OSM API', err);
+      return null;
+    });
+
+  clearTimeout(timeoutId);
+
+  return result;
+}
+
+/**
+ * Converts degrees to radians.
+ * @param degrees the degrees to convert
+ *
+ * @author Kerbourc'h
+ */
+function degreesToRadians(degrees) {
+  return (degrees * Math.PI) / 180;
+}
+
+/**
+ * Calculates the distance between two GPS coordinates.
+ * @param start the starting position
+ * @param end the ending position
+ * @return the distance between the two GPS coordinates in meters
+ *
+ * @author Kerbourc'h
+ */
+function distanceBetweenNodes(start: LatLng, end: LatLng) {
+  let lat1 = start.lat;
+  const lon1 = start.lng;
+  let lat2 = end.lat;
+  const lon2 = end.lng;
+
+  const earthRadiusKm = 6371;
+
+  const dLat = degreesToRadians(lat2 - lat1);
+  const dLon = degreesToRadians(lon2 - lon1);
+
+  lat1 = degreesToRadians(lat1);
+  lat2 = degreesToRadians(lat2);
+
+  const a =
+    Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+    Math.sin(dLon / 2) * Math.sin(dLon / 2) * Math.cos(lat1) * Math.cos(lat2);
+  const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+  return earthRadiusKm * c * 1000;
+}
+
+/**
+ * Gets the ways in a Road using one way id of the road.
+ * @param OSMWayId the id of a way in the road
+ * @param callback the callback function to call with the result
+ *
+ * @author Kerbourc'h
+ */
+export async function getOSMWaysInARoad<T>(
+  OSMWayId: string,
+  callback: (data: IWay[]) => T,
+): Promise<T> {
+  const query = `[out:json][timeout:${OSM_API_TIMEOUT}];
+  way(${OSMWayId});
+  foreach((._;complete{way[highway][name](around:100)(if:t["name"]==u(t["name"]));};);
+         way._[highway~"^(motorway|trunk|pedestrian|primary|secondary|tertiary|unclassified|service|residential|living_street)(_link)?$"];
+             
+  (._;.result;)->.result;);.result;
+  out geom;`;
+  return await queryOSM(query, (data) => {
+    let ways: IWay[] = [];
+    for (const element of data.elements) {
+      if (element.type === 'way') {
+        let length = 0;
+        for (let i = 0; i < element.nodes.length - 1; i++) {
+          const start: LatLng = {
+            lat: element.geometry[i].lat,
+            lng: element.geometry[i].lon,
+          };
+          const end: LatLng = {
+            lat: element.geometry[i + 1].lat,
+            lng: element.geometry[i + 1].lon,
+          };
+          length += distanceBetweenNodes(start, end);
+        }
+
+        ways.push({
+          osm_id: element.id,
+          way_name: element.tags.name,
+          section_geom: {
+            type: 'LineString',
+            coordinates: element.geometry.map((LatLon: any) => {
+              return [LatLon.lon, LatLon.lat];
+            }),
+          },
+          node_start: element.nodes[0],
+          node_end: element.nodes[element.nodes.length - 1],
+          length: length,
+          isoneway: false,
+        });
+      }
+    }
+
+    return callback(ways);
+  });
+}
--- backend/src/roads/road.service.ts
+++ backend/src/roads/road.service.ts
@@ -1,13 +1,136 @@
 import { Injectable } from '@nestjs/common';
 import { InjectConnection, Knex } from 'nestjs-knex';
-import { Ways } from '../tables';
-import { LatLng, Road, WayId } from '../models';
+import { IWay, Way } from '../tables';
+import { LatLng, OSMWayId, Road } from '../models';
+import { getOSMWaysInARoad } from './osm';
 
 @Injectable()
 export class RoadService {
   constructor(
-    @InjectConnection('lira-map') private readonly knex_liramap: Knex,
+    @InjectConnection('group-d') private readonly knex_groupd: Knex,
   ) {}
+
+  /**
+   * Insert a way in the database.
+   * @param way the way to insert
+   * @returns the inserted way (in an array)
+   *
+   * @author Kerbourc'h
+   */
+  async insertWay(way: IWay) {
+    const { section_geom, ...rest } = way;
+
+    return Way(this.knex_groupd)
+      .insert({
+        ...rest,
+        section_geom: this.knex_groupd.raw('ST_GeomFromGeoJSON(?)', [
+          JSON.stringify(section_geom),
+        ]),
+      })
+      .onConflict('osm_id')
+      .merge() // Force to update because returning doesn't work with ignore()
+      .returning([
+        'id',
+        'way_name',
+        'osm_id',
+        'node_start',
+        'node_end',
+        'length',
+        this.knex_groupd.raw(
+          'ST_AsGeoJSON(section_geom)::json as section_geom',
+        ),
+        'isoneway',
+      ]);
+  }
+
+  /**
+   * Get or create the ways corresponding to the OSMWayIds.
+   * You should definitely use this function instead of getOrCreateWay if you
+   * have multiple ways to get.
+   * This executes the queries sequentially to avoid useless queries to OSM
+   * and the database. This is especially useful when the ways are in the same road.
+   *
+   * // TODO: change this a nest queue
+   *
+   * @param wayIds
+   *
+   * @author Kerbourc'h
+   */
+  async getOrCreateWays(wayIds: OSMWayId[]) {
+    let tasks: any = wayIds.map((id, index) =>
+      index === 0 ? this.getOrCreateWay(id).then((value: IWay) => [value]) : id,
+    );
+
+    return await tasks.reduce(async (cur: Promise<IWay[]>, next: string) => {
+      return cur.then(async (value: IWay[]) => {
+        if (((value.length / tasks.length) * 100) % 10 === 0)
+          console.info(
+            'Importation status',
+            `${(value.length / tasks.length) * 100}%`,
+          );
+
+        return [...value, await this.getOrCreateWay(next)];
+      });
+    });
+  }
+
+  /**
+   * Will get the Way corresponding to the OSMWayId either from the database if
+   * available or from OSM and insert the whole road containing it in the database.
+   *
+   * // TODO: change this a nest queue
+   *
+   * @param OSMWayId the osm id of a way in the road
+   *
+   * @author Kerbourc'h
+   */
+  async getOrCreateWay(OSMWayId: OSMWayId): Promise<IWay> {
+    const way = await Way(this.knex_groupd)
+      .select(
+        'id',
+        'way_name',
+        'osm_id',
+        'node_start',
+        'node_end',
+        'length',
+        this.knex_groupd.raw(
+          'ST_AsGeoJSON(section_geom)::json as section_geom',
+        ),
+        'isoneway',
+      )
+      .where('osm_id', OSMWayId)
+      .limit(1);
+
+    // If the way doesn't exist in the database, fetch the road from OSM
+    // and insert it in the database. Then return the way.
+    if (way.length === 0) {
+      console.info(
+        `Way ${OSMWayId} doesn't exist in the database, fetching road from OSM.`,
+      );
+      return await getOSMWaysInARoad(OSMWayId, async (data): Promise<IWay> => {
+        if (data.length > 0) {
+          console.info(
+            `Inserting road containing ${data.length} ways in the database.`,
+          );
+          return await Promise.all(data.map((way) => this.insertWay(way))).then(
+            async (result): Promise<IWay> => {
+              const ways = result.flat();
+              console.debug(
+                "Road's OSM Ids:",
+                ways.map((way) => way.osm_id).join(', '),
+              );
+              return ways.filter((way) => way.osm_id === OSMWayId)[0];
+            },
+          );
+        } else {
+          console.error('No way found in the road. Check the OSM Id.');
+          return null;
+        }
+      });
+    }
+
+    return way[0];
+  }
 
   /**
    * Query the geometry, node_start and node_end of each way and structure the data properly
