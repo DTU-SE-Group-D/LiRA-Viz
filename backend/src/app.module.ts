aaf4aec3cfdcbc71da897aefc5ee70a145b6126d
liulw2022 <liwei_liu_@outlook.com>
2023-11-08 17:23:35 +0100

    serve static

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { KnexModule } from 'nestjs-knex';
 import { ConfigModule, ConfigService } from '@nestjs/config';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-
 import { RCController } from './conditions/rc.controller';
 import { RCService } from './conditions/rc.service';
 
@@ -26,6 +27,13 @@ const database = (config: any, name: string) => {
     ConfigModule.forRoot(),
     database(DB_LIRAMAP_CONFIG, 'lira-map'),
     database(DB_GROUPD_CONFIG, 'group-d'),
+    ServeStaticModule.forRoot({
+      rootPath:
+        process.env.IMAGE_PATH_FOR_DEV === 'true'
+          ? join(__dirname, '..', '..', '..', 'images')
+          : '/home/fish/images/',
+      serveRoot: '/images/',
+    }),
   ],
   controllers: [AppController, RCController, RoadController],
   providers: [AppService, ConfigService, RCService, RoadService],
