2d126700b80c734f7b4ac6c7444924038c6bd1f0
a-thansen <102360418+a-thansen@users.noreply.github.com>
2023-12-12 16:44:33 +0100

    Create progressbar.tsx

---
+++ frontend/src/Components/progressbar.tsx
@@ -0,0 +1,20 @@
+// import React from "react";
+
+// interface ProgressBarProps {
+
+// }
+
+// /**
+//  * MultiSelector is a component that is a dropdown list
+//  * that allows for multiple selection
+//  *
+//  * @author Hansen
+//  */
+// const ProgressBar: React.FC<ProgressBarProps> = ({
+
+// }) => {
+
+//     return (
+
+//     );
+// }
