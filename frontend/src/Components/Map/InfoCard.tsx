b034fb7adcf8748da1cd83267a6777affb4e225d
Sol2526 <45044551+Sol2526@users.noreply.github.com>
2023-12-15 13:01:45 +0100

    Select Survey & Inspect (#165)

--- frontend/src/Components/Map/InfoCard.tsx
+++ frontend/src/Components/Map/InfoCard.tsx
@@ -2,50 +2,72 @@ import React, { useCallback } from 'react';
 import { useNavigate } from 'react-router-dom';
 import '../../css/roadinfo_card.css';
 import { IRoad } from '../../models/path';
+import { ISurvey } from '../../../../backend/src/models';
 
 interface Props {
   /** To control visibility */
   hidden: boolean;
   /** The road data to display */
   roadData?: IRoad;
+  /** The survey data to display */
+  surveyData?: ISurvey;
 }
 /**
  * A components that renders the info card when users click on a road with data
- *
- * @author Chen
+ * Additionally, when selecting a survey, an info card will appear with survey ID & timestamp
+ * @author Chen, Lyons
  */
-const InfoCard: React.FC<Props> = ({ hidden, roadData }) => {
+const InfoCard: React.FC<Props> = ({ hidden, roadData, surveyData }) => {
   const navigate = useNavigate(); // Get the navigate function
 
-  // Navigate to the "Inspect" page
   const handleInspect = useCallback(() => {
-    if (!roadData) return;
-    const biggestBranch = roadData?.branches.reduce((prev, curr) =>
-      prev.length > curr.length ? prev : curr,
-    );
+    // Determine whether to navigate to road or survey inspect page
+    if (roadData) {
+      const biggestBranch = roadData.branches.reduce((prev, curr) =>
+        prev.length > curr.length ? prev : curr,
+      );
+      navigate('/inspect/paths/' + biggestBranch.join(','), {
+        state: { name: roadData.way_name },
+      });
+    } else if (surveyData) {
+      navigate(`/inspect/surveys/${surveyData.id}`, {
+        state: { name: new Date(surveyData.timestamp).toLocaleDateString() },
+      });
+    }
+  }, [roadData, surveyData, navigate]);
 
-    navigate('/inspect/paths/' + biggestBranch.join(','), {
-      state: { name: roadData.way_name },
-    });
-  }, [roadData, navigate]);
-
-  if (hidden || !roadData) {
-    return null; // Don't render anything if hidden is true
+  if (hidden || (!roadData && !surveyData)) {
+    return null;
   }
 
   return (
     <div className="roadinfo-card-content">
-      <div className="roadinfo-text-container">
-        <div className="roadinfo-card-text">
-          <span className="text-title">Road Name:</span>{' '}
-          <span className="card-road-name">{roadData.way_name}</span>
+      {roadData && (
+        <div className="roadinfo-text-container">
+          <div className="roadinfo-card-text">
+            <span className="text-title">Road Name:</span>{' '}
+            <span className="card-road-name">{roadData.way_name}</span>
+          </div>
+          <div className="roadinfo-card-text">
+            <span className="text-title"> Branch Number: </span>
+            <span className="card-road-length">{roadData.branches.length}</span>
+          </div>
         </div>
-        <div className="roadinfo-card-text">
-          <span className="text-title"> Branch Number: </span>
-          <span className="card-road-length"> {roadData.branches.length} </span>
+      )}
+      {surveyData && (
+        <div className="roadinfo-text-container">
+          <div className="roadinfo-card-text">
+            <span className="text-title">Survey ID:</span>
+            <span className="card-road-name">{surveyData.id}</span>
+          </div>
+          <div className="roadinfo-card-text">
+            <span className="text-title">Last updated:</span>
+            <span className="card-road-length">
+              {new Date(surveyData.timestamp).toLocaleDateString()}
+            </span>
+          </div>
         </div>
-      </div>
-
+      )}
       <button onClick={handleInspect} className="inspect-button">
         Inspect
       </button>
--- frontend/src/Components/Map/Inputs/Hamburger.tsx
+++ frontend/src/Components/Map/Inputs/Hamburger.tsx
@@ -1,8 +1,8 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import React, { useEffect, useRef, useState } from 'react';
 import '../../../css/Sidebar.css';
 import { getAllSurveyData } from '../../../queries/conditions';
-import { SurveyList } from '../../../../../backend/src/models';
+import { SurveyList, ISurvey } from '../../../../../backend/src/models';
+import InfoCard from '../InfoCard';
 
 interface HamburgerProps {
   /** Indicates if the sidebar is currently open. */
@@ -20,34 +20,75 @@ interface HamburgerProps {
  *
  * @author Lyons
  */
+
+const HEIGHT_OF_EACH_SURVEY_ITEM = 40; // Define this constant if not defined elsewhere
+
 const Hamburger: React.FC<HamburgerProps> = ({ isOpen, toggle }) => {
-  const [surveys, setSurveys] = useState<SurveyList>([]);
-  const navigate = useNavigate();
+  const [surveys, setSurveys] = useState<ISurvey[]>([]);
+  const [selectedSurvey, setSelectedSurvey] = useState<ISurvey | null>(null);
+  const [, setSelectedSurveyPosition] = useState<number>(0);
+
+  const sidebarRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    const handleClickOutside = (event: MouseEvent) => {
+      if (
+        isOpen &&
+        sidebarRef.current &&
+        !sidebarRef.current.contains(event.target as Node)
+      ) {
+        let node = event.target as Node | null;
+        let shouldCloseSidebar = true;
+
+        while (node instanceof HTMLElement) {
+          if (
+            node.classList.contains('hamburger') ||
+            node.classList.contains('inspect-button')
+          ) {
+            shouldCloseSidebar = false;
+            break;
+          }
+          node = node.parentNode;
+        }
+
+        if (shouldCloseSidebar) {
+          toggle();
+        }
+      }
+    };
+
+    document.addEventListener('mousedown', handleClickOutside);
+
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [isOpen, toggle]);
 
   useEffect(() => {
     getAllSurveyData((data: SurveyList) => {
-      setSurveys(data);
+      const transformedData: ISurvey[] = data.map((survey) => ({
+        id: survey.id,
+        timestamp: survey.timestamp,
+        geometry: [],
+        data: [],
+      }));
+      setSurveys(transformedData);
     });
   }, []);
 
-  const handleSurveyClick = useCallback(
-    (surveyId: string, index: number) => {
-      const path = `/inspect/surveys/${surveyId}`;
-      navigate(path, {
-        state: {
-          name: new Date(surveys[index].timestamp).toLocaleDateString(),
-        },
-      });
-    },
-    [navigate, surveys],
-  );
+  const handleSurveyClick = (surveyId: string, surveyIndex: number) => {
+    const survey = surveys.find((s) => s.id === surveyId);
+    setSelectedSurvey(survey || null);
+    const topPosition = surveyIndex * HEIGHT_OF_EACH_SURVEY_ITEM;
+    setSelectedSurveyPosition(topPosition);
+  };
 
   return (
     <>
       <div className="hamburger" onClick={toggle}>
         &#9776;
       </div>
-      <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+      <div ref={sidebarRef} className={`sidebar ${isOpen ? 'open' : ''}`}>
         <a href="#!" className="closebtn" onClick={toggle}>
           &times;
         </a>
@@ -57,7 +98,7 @@ const Hamburger: React.FC<HamburgerProps> = ({ isOpen, toggle }) => {
               <div
                 key={survey.id}
                 className="survey-block"
-                onClick={() => handleSurveyClick(survey.id, index)}
+                onClick={() => handleSurveyClick(survey.id, index + 1)}
               >
                 <h4>Survey {index + 1}</h4>
                 <p>Date: {new Date(survey.timestamp).toLocaleDateString()}</p>
@@ -66,6 +107,9 @@ const Hamburger: React.FC<HamburgerProps> = ({ isOpen, toggle }) => {
           )}
         </div>
       </div>
+      {selectedSurvey && (
+        <InfoCard hidden={!isOpen} surveyData={selectedSurvey} />
+      )}
     </>
   );
 };
